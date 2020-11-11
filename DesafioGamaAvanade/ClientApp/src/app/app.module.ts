267a5507f2ac23daa2471aa8c4462d5ff3e8a5d6
Moabe Renato <mrov@cin.ufpe.br>
2020-11-10 21:54:04 -0300

    modularizando as rotas

--- DesafioGamaAvanade/ClientApp/src/app/app.module.ts
+++ DesafioGamaAvanade/ClientApp/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutingModule } from './app.routing';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
@@ -18,18 +20,14 @@ import { LoginComponent } from './login/login.component';
     HomeComponent,
     CounterComponent,
     FetchDataComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: LoginComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'login', component: LoginComponent },
-    ])
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- DesafioGamaAvanade/ClientApp/src/app/register/register.component.ts
+++ DesafioGamaAvanade/ClientApp/src/app/register/register.component.ts
@@ -3,25 +3,35 @@ import { Router } from "@angular/router";
 import { HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
 })
-export class LoginComponent {
+export class RegisterComponent {
   isExpanded = false;
   public userName: string;
   public password: string;
+  public sexo: string;
   private baseUrl: string;
   private logou: boolean;
 
   constructor(private http: HttpClient, @Inject('BASE_URL') baseUrl: string, private router: Router) {
     this.baseUrl = baseUrl;
+    this.sexo = 'masc';
   }
 
-  submit() {
+  submit(stg) {
     this.http.post<string>(this.baseUrl + 'login', `{username: ${this.userName}, password: ${this.password} }`).subscribe(result => {
       this.logou = true
       this.router.navigateByUrl('/fetch-data');
     }, error => { console.error(error.status); });
+  }
+
+  collapse() {
+    this.isExpanded = false;
+  }
+
+  toggle() {
+    this.isExpanded = !this.isExpanded;
   }
 }
